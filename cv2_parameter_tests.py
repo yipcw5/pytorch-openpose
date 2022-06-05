b424250616b72911b1e613f0300d220b67d35e17
yipcw5 <yipcw9@gmail.com>
2022-06-05 18:24:40 +0100

    file with loop to test resize/blur parameters

--- cv2_parameter_tests.py
+++ cv2_parameter_tests.py
@@ -1,3 +1,5 @@
+# Carry out keypoint detection on input image(s)
+
 import cv2
 import matplotlib.pyplot as plt
 import copy
@@ -10,22 +12,25 @@ from src.hand import Hand
 from google.colab.patches import cv2_imshow
 
 hand_estimation = Hand('model/hand_pose_model.pth')
-#test_image = 'input/hand_real.png'
-test_image = 'output_images/out301.png'
 oriImg = cv2.imread(test_image)  # B,G,R order
 
-# bounding box for hand - hardcoded
-x = 525
-y = 1050
-w = 512
+if crop=='manual':
+  # bounding box for hand - hardcoded
+  x = 1250
+  y = 690
+  w = 512
+  cropped_hand = oriImg[y:y+w, x:x+w, :] # input for keypoint detection
+elif crop=='auto': pass
+else: cropped_hand = oriImg
 
-all_hand_peaks = [] # format appropriate for canvas drawing
-# loop removed - assumes only 1 hand exists
-cropped_hand = oriImg[y:y+w, x:x+w, :] # input for keypoint detection
+'''
+if image_processing:
+  # test parameters here
+  output_hand = cv2.resize(cropped_hand, (512,512))
+  output_hand = cv2.blur(output_hand, (8,8))
+  #output_hand = cv2.flip(output_hand, 1)
 
-# test parameters here
-output_hand = cv2.resize(cropped_hand, (128,128))
-output_hand = cv2.blur(cropped_hand, (15,15))
+else: output_hand = cropped_hand
 
 # detect hand keypoints
 peaks = hand_estimation(output_hand)
@@ -35,24 +40,26 @@ all_hand_peaks.append(peaks)
 canvas = copy.deepcopy(output_hand)
 canvas = util.draw_handpose(canvas, all_hand_peaks)
 
-# display original image with cropping/bounding box - debugging only
-#cv2.rectangle(oriImg, (x, y), (x+w, y+w), (0, 255, 0, 255), 2)
-#cv2_imshow(oriImg) #cv.imshow 'causes Jupyter sessions to crash' in Colab
+if crop != 'False' and show_crop:
+  # display original image with cropping/bounding box - debugging only
+  cv2.rectangle(oriImg, (x, y), (x+w, y+w), (0, 255, 0, 255), 2)
+  cv2_imshow(oriImg) #cv.imshow 'causes Jupyter sessions to crash' in Colab
 
 print(peaks)
 plt.imshow(canvas[:, :, [2, 1, 0]])
 plt.axis('off')
 plt.show()
-
 '''
 sizes = [(128,128), (256,256), (512,512), (768,768), (1024,1024)]
-blurs = [(13,13),(17,17),(21,21)]#[(1,1), (3,3), (5,5), (7,7), (9,9)]
+blurs = [(1,1),(3,3),(5,5),(7,7),(9,9)]
 
 for s in sizes:
   for b in blurs:
-# test parameters here
-    output_hand = cv2.resize(cropped_hand, s)
-    output_hand = cv2.blur(cropped_hand, b)
+    all_hand_peaks = [] # format appropriate for canvas drawing
+    
+    # test parameters here
+    resized_hand = cv2.resize(cropped_hand, s)
+    output_hand = cv2.blur(resized_hand, b)
 
     # detect hand keypoints
     peaks = hand_estimation(output_hand)
@@ -71,4 +78,3 @@ for s in sizes:
     plt.imshow(canvas[:, :, [2, 1, 0]])
     plt.axis('off')
     plt.show()
-'''
